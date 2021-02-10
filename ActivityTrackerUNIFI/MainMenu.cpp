4553a80e1955c48d6a2c68625c8648a411877104
gybs99 <niccolo.redi@stud.unifi.it>
2021-02-10 21:23:01 +0100

    Added Intro text

--- ActivityTrackerUNIFI/MainMenu.cpp
+++ ActivityTrackerUNIFI/MainMenu.cpp
@@ -3,13 +3,26 @@
 //
 
 #include "MainMenu.h"
+#include <ctime>
 
-MainMenu::MainMenu() : wxFrame(nullptr, wxID_ANY, "Activity Tracker", wxPoint(30, 30), wxSize(800,600)) {
+MainMenu::MainMenu() : wxFrame(nullptr, wxID_ANY, "Activity Tracker", wxPoint(30, 30),
+                               wxSize(800,600)) {
 
-    mainMenuSizer = new wxBoxSizer(wxHORIZONTAL);
+    mainMenuSizer = new wxBoxSizer(wxVERTICAL);
 
-    introText = new wxStaticText(this, wxID_ANY, "Welcome, here you'll find a list of your daily tasks");
-    mainMenuSizer -> Add(introText, 0, wxEXPAND);
+    introText = new wxStaticText(this, wxID_ANY, "Welcome, here you'll find a list of your daily tasks!");
+    introText -> SetFont(wxFont(20,wxROMAN, wxNORMAL, wxNORMAL));
+
+    time_t currentTime = time(nullptr);
+    struct tm* timeStruct = localtime((&currentTime));
+
+    dayText = new wxStaticText(this, wxID_ANY, "Today is " + std::to_string(timeStruct->tm_mday) + '/' +
+                                std::to_string(timeStruct->tm_mon + 1) + '/' + std::to_string(timeStruct->tm_year + 1900));
+    dayText -> SetFont(wxFont(20,wxROMAN, wxNORMAL, wxNORMAL));
+
+
+    mainMenuSizer -> Add(introText, 0, wxEXPAND | wxALL, 20);
+    mainMenuSizer -> Add(dayText, 0 , wxEXPAND | wxALL, 20);
 
     this -> SetSizer(mainMenuSizer);
 }
--- ActivityTrackerUNIFI/MainMenu.h
+++ ActivityTrackerUNIFI/MainMenu.h
@@ -17,6 +17,7 @@ public:
 private:
     wxBoxSizer* mainMenuSizer;
     wxStaticText* introText;
+    wxStaticText* dayText;
 };
 
 
